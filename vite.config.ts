7b98f19abf96305079fdd3f47e3902060557caa6
nttruong21 <nttruong1006@gmail.com>
2026-07-27 14:32:06 +0700

    chore: update project name

--- vite.config.ts
+++ vite.config.ts
@@ -31,7 +31,6 @@ export default defineConfig({
       // },
     }),
     react(),
-    // please see https://tanstack.com/start/latest/docs/framework/react/guide/hosting#nitro for guides on hosting
     nitro(),
   ],
   resolve: {
