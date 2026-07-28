82b9aa1245d4bf0a55e8357b4be1bd5a9a27e28f
nttruong21 <nttruong1006@gmail.com>
2026-07-28 21:05:25 +0700

    feat: rebuild

--- lint-staged.config.ts
+++ lint-staged.config.ts
@@ -1,10 +1,7 @@
 import type { Configuration } from 'lint-staged'
 
 const config: Configuration = {
-  '**/*.{js,jsx,ts,tsx,json,jsonc,css,scss,md,mdx}': [
-    'bun run fix',
-    'bun run check',
-  ],
+  '**/*.{js,jsx,ts,tsx,json,jsonc,css,scss,md,mdx}': ['bun run check'],
 }
 
 export default config
