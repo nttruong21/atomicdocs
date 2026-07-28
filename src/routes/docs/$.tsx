cc4bde4bf04495be6f22a66db97375cc376bfeb5
nttruong21 <nttruong1006@gmail.com>
2026-07-28 21:42:47 +0700

    feat: rebuild

--- src/routes/docs/$.tsx
+++ src/routes/docs/$.tsx
@@ -20,7 +20,7 @@ import { source } from '@/lib/source'
 
 export const Route = createFileRoute('/docs/$')({
   component: Page,
-  ssr: false,
+  ssr: 'data-only',
   loader: async ({ params }) => {
     const slugs = params._splat?.split('/') ?? []
     const data = await serverLoader({ data: slugs })
