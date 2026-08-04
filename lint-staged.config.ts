cfe2c15b2dd9dc272b5c4e0ecb897a01807e72d1
nttruong21 <nttruong1006@gmail.com>
2026-08-04 11:24:06 +0700

    docs(alert dialog): add doc mdx file

--- lint-staged.config.ts
+++ lint-staged.config.ts
@@ -1,10 +1,7 @@
 import type { Configuration } from 'lint-staged'
 
 const config: Configuration = {
-  '**/*.{js,jsx,ts,tsx,json,jsonc,css,scss,md,mdx}': [
-    'bun run fix',
-    'bun run check',
-  ],
+  '**/*': ['bun run fix', 'bun run check'],
 }
 
 export default config
